ea6982e5f8c136d037c4e8c474e389423b064ce3
Kelpins <67246580+Kelpins@users.noreply.github.com>
2023-01-29 16:02:25 -0800

    Minor Bug Fixes

--- InsiteCode/app.py
+++ InsiteCode/app.py
@@ -147,30 +147,3 @@ def about():
 @app.route('/privacy-policy')
 def privacy():
     return render_template('privacy-policy.html.j2', **privacyVars)
-
-"""
-@app.route('/test', methods=["POST"])
-def test():
-    #Receives the variables for server side validation and in case they are to be used later(for emailing and such)
-    username = request.values.get("uname")
-    pwd = request.values.get("pwd")
-    fname = request.values.get("fname")
-    lname = request.values.get("lname")
-    name = fname + " " + lname
-    dob = request.values.get("dob")
-    email = request.values.get("email")
-    phonenum = request.values.get("phnnum")
-    #reformatting phone number
-    phonenum = "(" + phonenum[0:3] + ") " + phonenum[3:6] + "-" + phonenum[6:10]
-    termsandconditions = request.values.get("terms")
-    emails = request.values.get("emails")
-
-    #Server side validation - if they left one form empty or turned off the terms and conditions checkbox but somehow got to this point in the code it sends them to the hacker page
-    if username == "" or pwd == "" or fname == "" or lname == "" or dob == "" or email == "" or phonenum == "" or termsandconditions != "on":
-        return render_template('hacker.html.j2')
-
-    #Routes to IQ test
-    terms = generateSeries(5)
-
-    return render_template('test.html.j2', terms=terms, username=username, pwd=pwd, name=name, dob=dob, email=email, phonenum=phonenum, termsandconditions=termsandconditions, emails=emails)
-"""
