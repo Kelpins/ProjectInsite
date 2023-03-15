d91f9923b60f79bd8a16055515e637ddc9d50cba
TTLM <noodles.jd@hotmail.com>
2023-03-15 12:13:36 -0700

    Update forms.py

--- InsiteCode/forms.py
+++ InsiteCode/forms.py
@@ -1,9 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FormField, FieldList, validators
+from wtforms import Form, StringField, SubmitField, FormField, FieldList, validators
 
-class BigForm(FlaskForm):
-    index = FormField(IndexForm)
-    about = FormField(AboutForm)
 
 class IndexForm(FlaskForm):
     site_name = StringField('Site Name', [validators.Length(max=40)])
@@ -11,13 +8,34 @@ class IndexForm(FlaskForm):
     twitter = StringField('Twitter Link')
     email = StringField('Email')
     left_col = StringField('Left Column Header')
-    paragraphs = FieldList(StringField(), min=0)
-    # Fields for paragraphs: tie number to the data from content JSONs
+    # Paragraphs: tie min number to the data from content JSONs
+    paragraphs = FieldList(StringField(), min=9)
 
+class CardForm(Form):
+    heading = StringField('Heading')
+    text = StringField('Text')
+    
 
 class AboutForm(FlaskForm):
     head_blurb = StringField('Head Blurb')
-    
+    body_title = StringField('Body Title')
+    body_subtitle = StringField('Body Subtitle')
+    # About page has 6 cards by default
+    cards = FieldList(FormField(CardForm), min=6)
 
-# class PrivacyForm(FlaskForm):
-    
+class PrivacyForm(FlaskForm):
+    head_blurb = StringField('Head Blurb')
+    body_text = StringField('Body Text')
+
+class NewPageForm(FlaskForm):
+    page_title = StringField('Page Title')
+    page_header = StringField('Page Header')
+    page_subheader = StringField('Page Subheader')
+    text_header = StringField("Text Header")
+    text_subheader = StringField("Text Subheader")
+    paragraphs = FieldList(StringField(), min=9)
+
+class BigForm(FlaskForm):
+    index = FormField(IndexForm)
+    about = FormField(AboutForm)
+    privacy = FormField(PrivacyForm)
