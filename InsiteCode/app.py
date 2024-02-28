8942cf7aaf35f41f7dfd3426bb92b52c52584777
TTLM <noodles.jd@hotmail.com>
2024-02-28 14:56:22 -0800

    Fix registration and logout
    - Add login/logout buttons to wtform view
    
    - Add redirect away from login page if user is already authenticated
    
    - Add second test user, username = 'kellan2', password = 'bjork'

--- InsiteCode/app.py
+++ InsiteCode/app.py
@@ -52,6 +52,8 @@ for page in data["new"].keys():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('wtform'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
